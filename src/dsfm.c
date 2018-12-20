a3ae03a8709f616f424108318152fca845b9a295
khadijabenali <khadija.benali94@gmail.com>
2018-12-20 18:42:57 +0100

    menu

--- src/dsfm.c
+++ src/dsfm.c
@@ -32,19 +32,65 @@ int main( int argc, char **argv ) {
     	printf("Rang : %d\n Machine : %s\n Serveur de stockage 2 \n----------\n", k, name);	
 	else if(k == SERVER_3)
     	printf("Rang : %d\n Machine : %s\n Serveur de stockage 3 \n----------\n", k, name); 
+	
+
+
+	int choice=0;
+	int c;
+	char nomFichier [10];
+
+	//if(k == CUSTOMER){
+		while(!choice){
+			if(k == CUSTOMER){
+				printf("1. put \n");
+				printf("2. get \n");
+				printf("3. quitter \n");
+				scanf("%d",&c);
+				getchar();
+				MPI_Bcast(&c, 1, MPI_INT, CUSTOMER, MPI_COMM_WORLD);
+			}
+			MPI_Barrier(MPI_COMM_WORLD);
+		switch(c)
+		{
+			case 1: 
+				if(k == CUSTOMER){
+					printf("entrer un nom de fichier pour put\n");
+					//scanf("%s",nomFichier);
+					fgets(nomFichier, sizeof nomFichier, stdin);
+					//getchar();
+					printf("LE fichier a put : %s",nomFichier);
+				}
+				put(nomFichier);
+				MPI_Barrier(MPI_COMM_WORLD);
+				if(k == CUSTOMER)			
+					printf("Votre fichier a bien été stocké côté serveur \n----------\n");
+				break;
+			case 2:printf("entrer un nom de fichier pour get\n");
+				fgets(nomFichier, sizeof nomFichier, stdin);
+				//getchar();
+				get(nomFichier);
+				printf("L'opération de récupération a été effectué avec succès: \n Votre fichier se trouve sous le répértoire %s \n----------\n",ROOT_DIR);
+				break;
+			case 3: printf("Quitter \n");
+				choice = 1;
+				break;
+			default: if(k==CUSTOMER) printf("Wrong choice \n"); break;
+		}
+	}
+	//}
 
 	//Le client (rang = 0) envoie le fichier en block aux serveurs de stockage ( rang = 1 , rang = 2, rang = 3)
-    put(FILENAME);
+  /*  put(FILENAME);
 	if(k==0)
-		printf("Votre fichier a bien été stocké côté serveur \n----------\n");
+		printf("Votre fichier a bien été stocké côté serveur \n----------\n");*/
 	//Récupérer les blocs depuis les serveurs
 	/*char buffer[SIZE_BUFFER];
 	MPI_Barrier(MPI_COMM_WORLD);
 	get(getCountBlocs(FILENAME) + 1, FIRST_SERVER,buffer);*/
 	//MPI_Barrier(MPI_COMM_WORLD);
-	get(FILENAME);
+/*	get(FILENAME);
 	if(k==0)
-		printf("L'opération de récupération a été effectué avec succès: \n Votre fichier se trouve sous le répértoire %s \n----------\n",ROOT_DIR);
+		printf("L'opération de récupération a été effectué avec succès: \n Votre fichier se trouve sous le répértoire %s \n----------\n",ROOT_DIR);*/
     MPI_Finalize();                                 //End MPI
     
     return EXIT_SUCCESS;
